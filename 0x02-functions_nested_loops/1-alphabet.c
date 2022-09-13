3f4f524c020bae97d837130eac84a35e62504918
Toritseju Joan Okotie <joanokotie@yahoo.co.uk>
2022-09-13 14:30:49 +0100

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,14 +8,14 @@
 *
 * Return: Always 0.
 */
+
 void print_alphabet(void)
 {
 	char a;
-
 	for (a = 'a'; a <= 'z'; a++)
 	{
-		putchar(a);
+		_putchar(a);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
