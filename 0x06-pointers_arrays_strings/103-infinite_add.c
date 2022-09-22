a75e4321dfa4d69394dd7c7f96807a2ee80e6065
Toritseju Joan Okotie <joanokotie@yahoo.co.uk>
2022-09-22 22:21:18 +0100

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,71 +1,33 @@
 #include "main.h"
 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /**
-* infinite_add - adds two numbers.
-*
-* @n1: first parmeter
-* @n2: second parameter
-* @r: third parameter
-* @size_r: fourth parameter
-*
-* Return: pointer to the result.
-*/
+ * infinite_add - Adds two numbers.
+ * @n1: The first number to be added.
+ * @n2: The second number to be added.
+ * @r: The buffer to store the result.
+ * @size_r: The buffer size.
+ *
+ * Return: If r can store the sum - a pointer to the result.
+ *         If r cannot store the sum - 0.
+ */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i1, i2, tempi, ji, i, j, k1, k2, m;
-    
-    char temp[10000];
+	int index, n1_len = 0, n2_len = 0;
 
-    ji = i = i1 = i2 = j = k1 = k2 = m = 0;
-    while (n1[i1] != '\0')
-        i1++;
-	while (n2[i2] != '\0')
-        i2++;
-	if (i1 + 2 > size_r || i2 + 2 > size_r)
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
-	i1--;
-	i2--;
-	while (i <= i1 || i <= i2)
-	{
-		k1 = k2 = 0;
-		if (i <= i1)
-			k1 = n1[i1 - i] - '0';
-		if (i <= i2 && (i2 - i) >= 0)
-			k2 = n2[i2 - i] - '0';
-		j = k1 + k2 + m;
-		if (j >= 10)
-		{
-			m = 1;
-			j -= 10;
-		}
-		else
-			m = 0;
-		r[i] = j + '0';
-		i++;
-		ji++;
-	}
-	if (m > 0)
-	{
-		r[i] = m + '0';
-		r[i + 1] = '\0';
-	}
-	i = tempi = 0;
-	while (i <= ji)
-	{
-		temp[i] = r[ji - i];
-		tempi++;
-		i++;
-	}
-	i = 0;
-	while (i < tempi)
-	{
-		if (r[i] == '\0')
-		{
-			break;
-		}
-		r[i] = temp[i];
-		i++;
-	}
-	return (r);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,51 +2,44 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints buffer
- *
- * @b: buffer
- * @size: size
- *
- * Return: void
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
  */
 
 void print_buffer(char *b, int size)
 {
-	int t, j, i;
+	int byte, index;
 
-	t = 0;
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (t < size)
-	{
-		j = size - t < 10 ? size - t : 10;
-		printf("%08x: ", t);
-		for (i = 0; i < 10; i++)
+		for (index = 0; index < 10; index++)
 		{
-			if (i < j)
-				printf("%02x", *(b + t + i));
-			else
+			if ((index + byte) >= size)
 				printf("  ");
-			if (i % 2)
-			{
+			else
+				printf("%02x", *(b + index + byte));
+			if ((index % 2) != 0 && index != 0)
 				printf(" ");
-			}
 		}
-		for (i = 0; i < j; i++)
+		for (index = 0; index < 10; index++)
 		{
-			int c = *(b + t + i);
-
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
+			if ((index + byte) >= size)
+				break;
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+			else
+				printf(".");
 		}
+		if (byte >= size)
+			continue;
 		printf("\n");
-		t += 10;
 	}
+	if (size <= 0)
+		printf("\n");
 }
